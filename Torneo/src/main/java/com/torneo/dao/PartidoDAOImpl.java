f5a6b295643211d4fae0be5147f5841853e8d4b1
Carlos Setien Cimas <csetienc@axpenet.local>
2017-08-09 09:47:50 +0200

    creador PARTIDODAO

---
+++ Torneo/src/main/java/com/torneo/dao/PartidoDAOImpl.java
@@ -0,0 +1,34 @@
+package com.torneo.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.torneo.dao.PartidoDAO;
+import com.torneo.model.Jugador;
+import com.torneo.model.Partido;
+
+public class PartidoDAOImpl implements PartidoDAO{
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public PartidoDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Partido> partidosJugadosPorFase(int fase) {
+		return (List<Partido>) sessionFactory.getCurrentSession().createQuery("from Partido").list();
+
+	}
+
+}
