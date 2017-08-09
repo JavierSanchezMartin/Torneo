913254bfa9387c3f3af89b88d252b5f75e1e7d56
Carlos Setien Cimas <csetienc@axpenet.local>
2017-08-09 09:03:37 +0200

    creado partidoService

--- Torneo/src/main/java/com/torneo/model/Partido.java
+++ Torneo/src/main/java/com/torneo/model/Partido.java
@@ -11,8 +11,8 @@ import javax.persistence.Table;
 public class Partido {
 
 	@Id
-    @GeneratedValue
-    @Column(name = "ID_PARTIDO")
+	@GeneratedValue
+	@Column(name = "ID_PARTIDO")
 	private int id_partido;
 	@Column(name = "ID_TORNEO")
 	private int id_torneo;
@@ -22,9 +22,10 @@ public class Partido {
 	private int equipo_visitante;
 	@Column(name = "FASE")
 	private String fase;
-	
-	public Partido() {}
-	
+
+	public Partido() {
+	}
+
 	public Partido(int id_partido, int id_torneo, int equipo_local, int equipo_visitante, String fase) {
 		this.id_partido = id_partido;
 		this.id_torneo = id_torneo;
@@ -32,33 +33,43 @@ public class Partido {
 		this.equipo_visitante = equipo_visitante;
 		this.fase = fase;
 	}
+
 	public int getId_partido() {
 		return id_partido;
 	}
+
 	public void setId_partido(int id_partido) {
 		this.id_partido = id_partido;
 	}
+
 	public int getId_torneo() {
 		return id_torneo;
 	}
+
 	public void setId_torneo(int id_torneo) {
 		this.id_torneo = id_torneo;
 	}
+
 	public int getEquipo_local() {
 		return equipo_local;
 	}
+
 	public void setEquipo_local(int equipo_local) {
 		this.equipo_local = equipo_local;
 	}
+
 	public int getEquipo_visitante() {
 		return equipo_visitante;
 	}
+
 	public void setEquipo_visitante(int equipo_visitante) {
 		this.equipo_visitante = equipo_visitante;
 	}
+
 	public String getFase() {
 		return fase;
 	}
+
 	public void setFase(String fase) {
 		this.fase = fase;
 	}
---
+++ Torneo/src/main/java/com/torneo/service/PartidoServiceImpl.java
@@ -0,0 +1,16 @@
+package com.torneo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.torneo.model.Partido;
+
+@Service
+public class PartidoServiceImpl implements PartidoService{
+
+	public List<Partido> generarRonda(int ronda){
+		List<Partido> partidos = null;
+		return partidos;
+	}
+
+}
